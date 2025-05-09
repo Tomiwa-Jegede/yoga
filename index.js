2a6843925fba3c2a2bb3a24f234cdca4ebefa4da
Tomiwa Jegede <jegedetomiwa1@gmail.com>
2025-05-10 00:27:51 +0100

    i made the browser remember the last mode that was selected

--- index.js
+++ index.js
@@ -3,9 +3,14 @@ const nightModeBtn = document.getElementsByClassName("darkmode-icon")[0];
 const cancel_btn = document.getElementsByClassName("nav-menu")[0];
 const menu_container = document.querySelector(".nav-menu");
 const menu = document.querySelector(".menu");
+if (localStorage.getItem('theme') === 'dark') { body.classList.add("darkmode") }
 //nightmode btn
 nightModeBtn.addEventListener("click", () => {
   body.classList.toggle("darkmode")
+  if (body.classList.contains("darkmode")) {
+    localStorage.setItem('theme', 'dark')
+  }
+  else { localStorage.setItem('theme', 'light') }
 });
 //cancel btn
 cancel_btn.addEventListener("click", () => {
